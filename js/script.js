5203ccf3f3d4c9fc0a556af6920edd2133a16060
Luiz166 <otavio.tata.morais@gmail.com>
2022-09-03 18:51:33 -0300

    redirecionamento na barra de pesquisa para a pagina do filme

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 document.querySelector("#search").addEventListener("keydown", function (e){
     if (e.keyCode == 13 && e.target.value == "American Psycho") {
         console.log(window.location.origin + "movies_pages/american_psycho/index.html");
-        window.location.href = window.location.origin + "/movies_pages/american_psycho/index.html";
+        if (window.location.origin.indexOf ("github") > 0){
+            window.location.href = "https://luiz166.github.io/movie_town/movies_pages/american_psycho/index.html";
+        }
+        else{
+            window.location.href = "http://127.0.0.1:5500/movies_pages/american_psycho/index.html";
+        }
     } else{
         console.log("nao");
     }

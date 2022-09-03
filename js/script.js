f3885d673abd8a4819ff1a468608bee7eed36702
Luiz166 <otavio.tata.morais@gmail.com>
2022-09-03 20:30:15 -0300

    adicionado redirecionamento da barra de pesquisa para a pagina dos filmes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
 document.querySelector("#search").addEventListener("keydown", function (e){
     if (e.keyCode == 13 && e.target.value == "American Psycho") {
-        console.log(window.location.origin + "movies_pages/american_psycho/index.html");
         if (window.location.origin.indexOf ("github") > 0){
             window.location.href = "https://luiz166.github.io/movie_town/movies_pages/american_psycho/index.html";
         }
         else{
             window.location.href = "http://127.0.0.1:5500/movies_pages/american_psycho/index.html";
         }
-    } else{
-        console.log("nao");
+    }
+    else if (e.keyCode == 13 && e.target.value == "Blade Runner 2049") {
+        if (window.location.origin.indexOf ("github") > 0){
+            window.location.href = "https://luiz166.github.io/movie_town/movies_pages/bladerunner2049/index.html";
+        }
+        else{
+            window.location.href = "http://127.0.0.1:5500/movies_pages/bladerunner2049/index.html";
+        }
+    }
+    else if (e.keyCode == 13 && e.target.value == "Drive") {
+        if (window.location.origin.indexOf ("github") > 0){
+            window.location.href = "https://luiz166.github.io/movie_town/movies_pages/drive/index.html";
+        }
+        else{
+            window.location.href = "http://127.0.0.1:5500/movies_pages/drive/index.html";
+        }
+    }
+    else if (e.keyCode == 13 && e.target.value == "NightCrawler") {
+        if (window.location.origin.indexOf ("github") > 0){
+            window.location.href = "https://luiz166.github.io/movie_town/movies_pages/nightcrawler/index.html";
+        }
+        else{
+            window.location.href = "http://127.0.0.1:5500/movies_pages/nightcrawler/index.html";
+        }
+    }
+    else if (e.keyCode == 13 && e.target.value == "Taxi Driver") {
+        if (window.location.origin.indexOf ("github") > 0){
+            window.location.href = "https://luiz166.github.io/movie_town/movies_pages/taxi_driver/index.html";
+        }
+        else{
+            window.location.href = "http://127.0.0.1:5500/movies_pages/taxi_driver/index.html";
+        }
+    }
+    else if (e.keyCode == 13 && e.target.value == "The Place Beyond the Pines") {
+        if (window.location.origin.indexOf ("github") > 0){
+            window.location.href = "https://luiz166.github.io/movie_town/movies_pages/tpbtp/index.html";
+        }
+        else{
+            window.location.href = "http://127.0.0.1:5500/movies_pages/tpbtp/index.html";
+        }
     }
 })
